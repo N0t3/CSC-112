6ed7cfb2d9176ec72cca1d087d5f88006b34f537
Chip Koch <chipgad@gmail.com>
2021-09-21 23:03:40 -0400

    Added more opperations to Main.java and changed the spacing in input.txt

--- assignmentOne/Main.java
+++ assignmentOne/Main.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.*;  
 
 public class Main {
     public static void main(String [] args) throws IOException {
-        String[] array;
         FileInputStream fileByteStream = new FileInputStream("input.txt");
         Scanner inputFile = new Scanner(fileByteStream);
-        while(inputFile.hasNextLine()){
-            readStudents(inputFile, array);
-        }
-    }
-    public static void readStudents(Scanner inputFile, String[] students){
-        for(int i = 0; i < students.length; i++){
-            students[i] = inputFile.nextLine();
-        }
-    }
 
+        BufferedReader lineReader = new BufferedReader(new FileReader("input.txt"));
+        int numOfLines = 0;
+        while (lineReader.readLine() != null) numOfLines++;
+        lineReader.close();
+
+        String[] students = new String[numOfLines];
+        int[] arrayAvgGrades = new int[numOfLines]; 
+
+        System.out.println("done");
+        while(inputFile.hasNextLine()){
+            readStudents(inputFile, students, arrayAvgGrades);
+            
+        }
+
+    }
+    public static void readStudents(Scanner inputFile, String[] students, int[] arrayAvgGrades){
+        for(int i = 0; i < students.length; i++){
+            if(inputFile.hasNextInt()){
+                arrayAvgGrades[i] = inputFile.nextInt();
+                System.out.println(arrayAvgGrades[i]);
+            }
+        }
+    }
 }
 
