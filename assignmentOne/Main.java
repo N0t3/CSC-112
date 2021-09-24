a96e67f6c6bb56256f79d70b5da5af36722ba0a5
Chip Koch <chipgad@gmail.com>
2021-09-23 22:44:36 -0400

    Redirected my approach using 2d arrays

--- assignmentOne/Main.java
+++ assignmentOne/Main.java
@@ -10,25 +10,41 @@ public class Main {
 
         BufferedReader lineReader = new BufferedReader(new FileReader("input.txt"));
         int numOfLines = 0;
-        while (lineReader.readLine() != null) numOfLines++;
-        lineReader.close();
+        int count = 0;
 
-        String[] students = new String[numOfLines];
-        int[] arrayAvgGrades = new int[numOfLines]; 
-
-
-        while(inputFile.hasNextLine()){
-            readStudents(inputFile, students, arrayAvgGrades);
-        }
-
-    }
-    public static void readStudents(Scanner inputFile, String[] students, int[] arrayAvgGrades){
-        for(int i = 0; i < students.length; i++){
+        while (lineReader.readLine() != null){
             if(inputFile.hasNextInt()){
-                arrayAvgGrades[i] = inputFile.nextInt();
+                count++;
             }
-            students[i] = inputFile.nextLine();
+            numOfLines++;
+        } 
+        lineReader.close();
+        System.out.println(numOfLines);
+        System.out.println(count);
+
+        // String[][] students = new String[numOfLines/count][count/6];
+
+        // System.out.println(numOfLines);
+
+        // for(int i = 0; i < students.length; i++){
+        //     for(int z = 0; z < students[i].length; z++){
+        //         System.out.println(students[i][z]);
+        //     }
+        // }
+
+        // while(inputFile.hasNextLine()){
+        //     readStudents(inputFile, students);
+        // }
+    }
+    public static void readStudents(Scanner inputFile, String[][] students){
+        for(int i = 0; i < students.length -1; i++){
+            for(int z = 0; z < students[i].length -1; z++){
+                students[i][z] = inputFile.nextLine();
+                System.out.println(students[i][z]);
+                System.out.println("done");
+            }
         }
     }
+    
 }
 
