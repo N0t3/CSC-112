5a1603f3cd596446ea06ea0356a8ba63575e3a0d
Chip Koch <chipgad@gmail.com>
2021-09-26 17:30:54 -0400

    Added the averageStudentGrade function

--- assignmentOne/Main.java
+++ assignmentOne/Main.java
@@ -25,6 +25,7 @@ public class Main {
         } 
        
         String[][] students = new String[countStudents +1][9];
+        double[] avereageTestGrade = new double[countStudents];
         
         while(inputFileTwo.hasNextLine()){
             readStudents(inputFileTwo, students);
@@ -36,6 +37,8 @@ public class Main {
             }
         }
         System.out.println(Arrays.deepToString(students));
+
+        averageTestGrade(students,avereageTestGrade);
     }
     
     public static void readStudents(Scanner x, String[][] students){
@@ -43,9 +46,19 @@ public class Main {
             for(int z = 0; z < students[i].length; z++){
                 if(x.hasNextLine()){
                     students[i][z] = x.next();
-                    //System.out.println(students[i][z]);
                 }
             }
+        }
+    }
+
+    public static void averageTestGrade(String[][] x, double[] array){
+        for(int i = 0; i < x.length -1; i++){
+            double cal = 0;
+            for(int z = 0; z < x[i].length -4; z++){
+                cal += Integer.parseInt(x[i][z + 4]);
+            }
+            array[i] = cal/5; 
+            System.out.println(array[i]);
         }
     }
     
